fdf47f004e606003efd138d0aeaec550b44cc40d
zainaraza43 <urboyzain@gmail.com>
2026-07-11 01:32:18 -0400

    silence experimental coroutines opt-in warning in test clock

--- riot4k-core/src/commonTest/kotlin/com/lowbudgetlcs/riot4k/core/SchedulerClock.kt
+++ riot4k-core/src/commonTest/kotlin/com/lowbudgetlcs/riot4k/core/SchedulerClock.kt
@@ -1,12 +1,14 @@
 package com.lowbudgetlcs.riot4k.core
 
 import com.lowbudgetlcs.riot4k.core.models.Clock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineScheduler
 
 /**
  * [Clock] that follows a `runTest` scheduler's virtual time, so `delay()` calls
  * inside the code under test advance the clock consistently.
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 class SchedulerClock(private val scheduler: TestCoroutineScheduler) : Clock {
     override fun nowMillis(): Long = scheduler.currentTime
 }
